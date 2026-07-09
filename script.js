246c8a197627437ba972385227b06b2e048316c4
Mariya Fain <fainmariya@gmail.com>
2026-07-09 09:24:29 +0300

    feat: add podcast search

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const loadMoreBtn = document.querySelector("#load-more-btn")
 const BASE_URL = "https://listen-api-test.listennotes.com/api/v2";
 let currentPage = 1;
 let nextPage=null;
-isLoading = false;
+let isLoading = false;
+let searchTimeout = null;
 
 function getBestPodcastsUrl(){
     
@@ -43,11 +44,18 @@ async function fetchBestPodcasts(){
 function renderPodcastCard(podcast){
     const podcastCard = document.createElement('div');
     podcastCard.classList.add('podcast__card');
-    podcastCard.innerHTML = `
-        <img src = "${podcast.image}" alt = "${podcast.title}">
-        <h3>${podcast.title}</h3>
-        <p>${podcast.publisher}</p>
+    let cardTitle = podcast.title || podcast.title_original;;
+    let cardPublisher = podcast.publisher || podcast.publisher_original;
+    let cardImage = podcast.image || podcast.thumbnail;
+    
+        podcastCard.innerHTML = `
+        <img src = "${cardImage}" alt = "${cardTitle}">
+        <h3>${cardTitle}</h3>
+        <p>${cardPublisher}</p>
     `
+   
+    
+    
       
     
     podcastsList.append(podcastCard)
@@ -61,4 +69,56 @@ loadMoreBtn.addEventListener('click',function(){
  fetchBestPodcasts()}
 
   })
+  searchInput.addEventListener("input",function(){
+    const searchValue  = searchInput.value.trim();
+
+    clearTimeout(searchTimeout);
+
+    searchTimeout = setTimeout(function(){
+        if (searchValue === "") {
+            podcastsList.innerHTML ="";
+            currentPage =1;
+            nextPage = null;
+            fetchBestPodcasts();
+
+            
+            return;
+          }
+          fetchSearchPodcasts(searchValue);
+  }, 1000);
+    })
+    
+function getSearchPodcastsUrl(query){
+    const encodedQuery = encodeURIComponent(query);
+    const searchUrl = `${BASE_URL}/search?q=${encodedQuery}&type=podcast`
+    return searchUrl
+
+
+}
+async function fetchSearchPodcasts(query) {
+    if (isLoading) return;
+
+    const urlSearchPodcast= getSearchPodcastsUrl(query);
+
+    isLoading = true;
+    loader.style.display = "block";
+
+    try {
+        const response = await fetch(urlSearchPodcast);
+        if (!response.ok) {
+            throw new Error("Failed to fetch podcasts");
+        }
+        const dataSearch = await response.json();
+        podcastsList.innerHTML = "";
+        dataSearch.results.forEach((result) => {
+            renderPodcastCard(result.podcast);
+          });
+        
+      } catch (error) {
+        console.error(error);
+      } finally {
+        loader.style.display = "none";
+        isLoading = false;
+      }
+}
   
