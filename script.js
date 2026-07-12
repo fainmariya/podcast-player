c68bb77569a4e2551457957fa6c0a7df3e0168a5
Mariya Fain <fainmariya@gmail.com>
2026-07-12 21:21:33 +0300

    feat: add global podcast player

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const loadMoreBtn = document.querySelector("#load-more-btn");
 const app = document.querySelector("#app");
 const homePage = document.querySelector("#home-page");
 const detailsPage = document.querySelector("#details-page");
+const audioPlayer = document.querySelector("#audio-player");
+const globalPlayer = document.querySelector("#global-player");
+const globalPlayerCover = document.querySelector("#global-player-cover");
+const globalPlayerTitle = document.querySelector("#global-player-title");
+const globalPlayerReturn = document.querySelector("#global-player-return");
+const globalPlayerClose = document.querySelector("#global-player-close");
 
+let currentPlayingEpisode = null;
 
 
 const BASE_URL = "https://listen-api-test.listennotes.com/api/v2";
@@ -172,27 +179,53 @@ function renderPodcastDetailsPage(podcastSearch, selectedPodcast){
     const detailsImage = selectedPodcast.image || selectedPodcast.thumbnail;
     const detailsTitle = selectedPodcast.title || selectedPodcast.title_original;
     const detailsPublisher = selectedPodcast.publisher || selectedPodcast.publisher_original;
+
+    const firstEpisode = podcastSearch.episodes[0];
+    let selectedEpisode = firstEpisode;
+    const firstEpisodeDate = new Date(firstEpisode.pub_date_ms).toLocaleDateString();
+    const firstEpisodeMinutes = Math.floor(firstEpisode.audio_length_sec / 60);
+    const firstEpisodeSeconds = firstEpisode.audio_length_sec % 60;
+
     detailsPage.innerHTML = `
-    <button id="back-to-home-btn" class="details__back">← Back</button>
+        <button id="back-to-home-btn" class="details__back">← Back</button>
 
-    <section class="details__hero">
-        <img src="${detailsImage}" alt="${detailsTitle}" class="details__image">
+        <section class="details__hero">
+            <img id="details-main-image" src="${detailsImage}" alt="${detailsTitle}" class="details__image">
 
-        <div class="details__info">
-            <h2>${detailsTitle}</h2>
-            <p class="details__publisher">${detailsPublisher}</p>
-            <p class="details__description">${podcastSearch.description}</p>
-        </div>
-    </section>
+           <div class="details__info">
+                <h2 id="details-main-title">${detailsTitle}</h2>
+                <p id="details-main-subtitle" class="details__publisher">${detailsPublisher}</p>
+                <p id="details-main-description" class="details__description">${podcastSearch.description}</p>
+
+                <button id="latest-episode-btn" class="details__play-btn">
+                    ▶ Start Listening
+                </button>
+
+                <p id="details-main-meta" class="details__episode-time">
+                    ${firstEpisodeDate} · ${firstEpisodeMinutes} min ${firstEpisodeSeconds} sec
+                </p>
+                
+            </div>
+        </section>
 
     <section class="episodes">
         <h3>Episodes</h3>
         <ul id="episodes-list" class="episodes__list"></ul>
     </section>
 `;
+    
+
+    const latestEpisodeBtn = document.querySelector("#latest-episode-btn");
+
+    latestEpisodeBtn.addEventListener('click', function(){
+
+        updateMainEpisodeInfo(selectedEpisode, detailsImage);
+        playEpisodeInGlobalPlayer(selectedEpisode, detailsImage);
+    });
     const listDetail = document.querySelector("#episodes-list");
     podcastSearch.episodes.forEach(function(episode){
         const li = document.createElement("li");
+        li.dataset.episodeId = episode.id;
         const episodeDate = new Date(episode.pub_date_ms).toLocaleDateString();
         const episodeMinutes = Math.floor(episode.audio_length_sec / 60);
         const episodeSeconds = episode.audio_length_sec % 60;
@@ -220,8 +253,15 @@ li.innerHTML = `
             <span>·</span>
             <span>${episodeMinutes} min ${episodeSeconds} sec</span>
         </div>
+        
     </div>
-`;
+    `;
+    const buttonPlay = li.querySelector(".episode__play-btn");
+    buttonPlay.addEventListener('click', function(){
+
+        updateMainEpisodeInfo(episode, detailsImage);
+        playEpisodeInGlobalPlayer(episode, detailsImage);
+    });
         listDetail.append(li);
     })
     const backBtn = document.querySelector("#back-to-home-btn")
@@ -231,6 +271,85 @@ li.innerHTML = `
     })
     
     }
+
+    function updateMainEpisodeInfo(episode, fallbackImage) {
+        const mainImage = document.querySelector("#details-main-image");
+        const mainTitle = document.querySelector("#details-main-title");
+        const mainSubtitle = document.querySelector("#details-main-subtitle");
+        const mainDescription = document.querySelector("#details-main-description");
+        const mainMeta = document.querySelector("#details-main-meta");
+    
+        const episodeImage = fallbackImage;
+        const episodeDate = new Date(episode.pub_date_ms).toLocaleDateString();
+        const episodeMinutes = Math.floor(episode.audio_length_sec / 60);
+        const episodeSeconds = episode.audio_length_sec % 60;
+    
+        const cleanDescription = episode.description
+            ? episode.description.replace(/<[^>]*>/g, "")
+            : "";
+    
+        const shortDescription = cleanDescription.length > 260
+            ? cleanDescription.slice(0, 260) + "..."
+            : cleanDescription;
+    
+        mainImage.src = episodeImage;
+        mainImage.alt = episode.title;
+        mainTitle.textContent = episode.title;
+        mainSubtitle.textContent = "Now playing";
+        mainDescription.textContent = shortDescription;
+        mainMeta.textContent = `${episodeDate} · ${episodeMinutes} min ${episodeSeconds} sec`;
+    }
+
+    function playEpisodeInGlobalPlayer(episode, coverImage) {
+        currentPlayingEpisode = episode;
+        
+    
+        globalPlayer.hidden = false;
+    
+        globalPlayerCover.src = coverImage;
+        globalPlayerCover.alt = episode.title;
+        globalPlayerTitle.textContent = episode.title;
+    
+        audioPlayer.src = episode.audio;
+        audioPlayer.load();
+        
+        audioPlayer.play().catch(function(error) {
+            console.error("Audio play failed:", error);
+            console.error("Problem audio URL:", episode.audio);
+        });
+    
+        document.querySelectorAll(".episode__item--active").forEach((item) => {
+            item.classList.remove("episode__item--active");
+        });
+    
+        const activeEpisodeItem = document.querySelector(`[data-episode-id="${episode.id}"]`);
+        if (activeEpisodeItem) {
+            activeEpisodeItem.classList.add("episode__item--active");
+        }
+    }
+    globalPlayerReturn.addEventListener("click", function () {
+        if (!currentPlayingEpisode) return;
+    
+        homePage.hidden = true;
+        detailsPage.hidden = false;
+    
+        const activeEpisodeItem = document.querySelector(`[data-episode-id="${currentPlayingEpisode.id}"]`);
+    
+        if (activeEpisodeItem) {
+            activeEpisodeItem.scrollIntoView({
+                behavior: "smooth",
+                block: "center"
+            });
+        }
+    });
+    globalPlayerClose.addEventListener("click", function () {
+        audioPlayer.pause();
+        audioPlayer.removeAttribute("src");
+        audioPlayer.load();
+    
+        globalPlayer.hidden = true;
+        currentPlayingEpisode = null;
+    });
     window.addEventListener("popstate", function () {
         homePage.hidden = false;
         detailsPage.hidden = true;
